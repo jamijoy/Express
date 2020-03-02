ba0b21470d4fdf8118a6a1c6e8245afe13f8791e
MdEstiakAhmed <estiak97@gmail.com>
2020-03-03 01:23:35 +0600

    post searching with AJAX completed

--- project/models/post_content.js
+++ project/models/post_content.js
@@ -44,7 +44,7 @@ module.exports = {
 		})
 	},
 	getAllFromSearch : function(string, callback){
-		var sql = "select post_id, post_text from post_content where post_text like '%' ? '%'";
+		var sql = "select user_details.username, post_type.post_type_name, post_content.post_id, post_content.post_text, post_content.post_image, post_status.post_status_name from post_content, post_status, post_type, post_info, user_details WHERE post_content.post_id = post_info.post_id AND user_details.username = (SELECT user_details.username WHERE user_details.user_id = post_info.user_id) AND post_type.post_type_name = (SELECT post_type.post_type_name WHERE post_type.post_type_id = post_info.post_type_id) AND post_status.post_status_name = (SELECT post_status.post_status_name WHERE post_info.post_status_id = post_status.post_status_id) AND post_status.post_status_name = 'approve' AND post_content.post_text LIKE '%' ? '%'";
 		db.getResults(sql, [string], function(results){
 			if(results.length > 0){
 				callback(results);
@@ -54,7 +54,7 @@ module.exports = {
 		});
 	},
 	getAllPost : function(callback){
-		var sql = "select post_id, post_text from post_content";
+		var sql = "select user_details.username, post_type.post_type_name, post_content.post_id, post_content.post_text, post_content.post_image, post_status.post_status_name from post_content, post_status, post_type, post_info, user_details WHERE post_content.post_id = post_info.post_id AND user_details.username = (SELECT user_details.username WHERE user_details.user_id = post_info.user_id) AND post_type.post_type_name = (SELECT post_type.post_type_name WHERE post_type.post_type_id = post_info.post_type_id) AND post_status.post_status_name = (SELECT post_status.post_status_name WHERE post_info.post_status_id = post_status.post_status_id) AND post_status.post_status_name = 'approve'";
 		db.getResults(sql, null, function(results){
 			if(results.length > 0){
 				callback(results);
