7ba3900ce7a3aa3baea5b337e9a21fbad604da9f
MdEstiakAhmed <estiak97@gmail.com>
2020-03-08 03:35:25 +0600

    time added in message and delete, warning, block request

--- project/models/account_block_request.js
+++ project/models/account_block_request.js
@@ -2,8 +2,11 @@ var db = require('./db');
 
 module.exports = {
     insert: function(user_id, callback){
-		var sql = "insert into account_block_request values(?,?)";
-		db.execute(sql, [user_id, "pending"], function(status){
+		var d = new Date();
+		var millisecond = d.getTime();
+		var d = null;
+		var sql = "insert into account_block_request values(?,?, ?)";
+		db.execute(sql, [user_id, "pending", millisecond], function(status){
 			if(status){
 				callback(true);
 			}else{
